5c2fc9637802b26488839881e218482b9871a520
JavierMondragoD <31265510+JavierMondragoD@users.noreply.github.com>
2017-11-27 23:17:55 -0600

    Este es mi trabajo de la materia estructura de datos del instituto tecnologico de cd altamirano del 3er semestre por el alumno Jose Javier Mondragon Delgado apoyado por el profesor L.I. Leonel Gonzales Vidales

---
+++ src/video51/Video51.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package video51;
+/**
+ *
+ * @author JOSE JAVIER
+ */
+public class Video51 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        AlgoritmosDeOrdenamiento ordenar=new AlgoritmosDeOrdenamiento();
+        int vector1[]={5,6,3,44,22,1);
+        int vector1[]={55,4,43,44,2,10);
+        System.out.println("Arreglo Orfiginal");
+        ordenar.MostrarArreglo(vector1);
+         System.out.println("Arreglo Ordenado con burbuja1");
+         ordenar.burbuja1(vector1);
+         ordenar.mostrarArreglo(vector1);
+          System.out.println("Arreglo Orfiginal");
+        ordenar.MostrarArreglo(vector2);
+         System.out.println("Arreglo Ordenado con burbuja2");
+         ordenar.burbuja1(vector2);
+         ordenar.mostrarArreglo(vector2);
+    }
+    }
+    
+}
